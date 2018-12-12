5d8892411394cfe377f3721cff1da4a749703f2c
Adrián Vera <vera.adrianj@gmail.com>
2018-12-12 17:45:06 -0400

    definición de relación de los profesores con los cursos

--- app/Course.php
+++ app/Course.php
@@ -8,4 +8,12 @@ class Course extends Model
 {
     protected $table = 'courses';
     protected $fillable = ['name'];
+
+    // Relaciones
+    
+    // Un curso es dictado por un [usuario] profesor
+    public function teacher()
+    {
+    	return $this->belongsTo(User::class);
+    }
 }
