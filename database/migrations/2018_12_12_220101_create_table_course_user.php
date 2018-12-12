aa98cf204ca0f5ebdd311ab253b96c0d689ab397
Adrián Vera <vera.adrianj@gmail.com>
2018-12-12 18:02:38 -0400

    creación de tabla pivot para la relación de estudiantes con los cursos

---
+++ database/migrations/2018_12_12_220101_create_table_course_user.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableCourseUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course_user', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('course_id')->unsigned();
+            $table->integer('student_id')->unsigned();
+            $table->double('qualification');
+
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('student_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course_user');
+    }
+}
