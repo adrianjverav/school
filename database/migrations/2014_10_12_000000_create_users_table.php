9c56781db68f6291bec677aa8fa4a0b89dd0fd00
Adrián Vera <vera.adrianj@gmail.com>
2018-12-11 23:14:35 -0400

    anexo de atributos a la tabla de usuarios

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,11 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('surname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'teacher', 'student']);
             $table->rememberToken();
             $table->timestamps();
         });
