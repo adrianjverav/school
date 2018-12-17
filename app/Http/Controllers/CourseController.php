dfeebd69fdb9d9a7b0ff9bbbe6afaf4153478521
Adrián Vera <vera.adrianj@gmail.com>
2018-12-17 15:46:48 -0400

    definición de métodos para todas las operaciones de los cursos

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Course;
+use App\User;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
@@ -14,8 +15,28 @@ class CourseController extends Controller
      */
     public function index()
     {
-        $courses = Course::getCourses(auth()->user());
-        return $courses;
+        $role = auth()->user()->role;
+        if ($role == 'admin') {
+            $courses = Course::with('teacher')->get();
+            return view('admin.courses.index', ['courses' => $courses]);
+        } else if ($role == 'student') {
+            $courses = auth()->user()->inscriptions;
+            return view('student.courses.index', ['courses' => $courses]);
+        } else {
+            $courses = auth()->user()->courses;
+            return view('teacher.courses.index', ['courses' => $courses]);
+        }
+    }
+
+    /**
+     * Display a listing of students of a curse.
+     * @param \App\Course $course
+     * @return \Illuminate\Http\Response
+     */
+    public function students(Course $course)
+    {
+        $enableChangeNote = Course::allowedTime();
+        return view('teacher.courses.students', ['course' => $course, 'enable' => $enableChangeNote]);
     }
 
     /**
@@ -26,6 +47,34 @@ class CourseController extends Controller
     public function create()
     {
         return view('admin.courses.create');
+    }
+
+    /**
+     * Show the form for update note a student.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function changeNote(Course $course, User $student)
+    {
+        return view('teacher.courses.change-note', ['course' => $course, 'student' => $student]);
+    }
+
+    /**
+     * Update the note of a student.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Course  $course
+     * @param  \App\User  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function updateNote(Request $request, Course $course, User $student)
+    {
+        $request->validate([
+            'note' => 'required|numeric|min:0|max:20',
+        ]);
+
+        $student->inscriptions()->updateExistingPivot($course->id, ['qualification' => $request->note]);
+        return redirect()->route('courses.students', [$course->id]);
     }
 
     /**
@@ -43,14 +92,55 @@ class CourseController extends Controller
     }
 
     /**
-     * Display the specified resource.
+     * Display list of teachers to be assigned to a course.
      *
      * @param  \App\Course  $course
      * @return \Illuminate\Http\Response
      */
-    public function show(Course $course)
+    public function assignTeacher(Course $course)
     {
-        return view('admin.courses.show', ['course' => $course]);
+        $teachers = User::where('role', 'teacher')->get();
+        return view('admin.courses.assign-teacher', ['course' => $course, 'teachers' => $teachers]);
+    }
+
+    /**
+     * Assign a teacher to a course.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Course  $course
+     * @return \Illuminate\Http\Response
+     */
+    public function saveTeacher(Request $request, Course $course)
+    {
+        $teacher = User::find($request->teacher_id);
+        $course->teacher()->associate($teacher);
+        $course->save();
+        return redirect()->route('courses.index');
+    }
+
+    /**
+     * Display list of teachers to be assigned to a course.
+     *
+     * @param  \App\Course  $course
+     * @return \Illuminate\Http\Response
+     */
+    public function addStudent(Course $course)
+    {
+        $students = User::where('role', 'student')->get();
+        return view('admin.courses.assign-student', ['course' => $course, 'students' => $students]);
+    }
+
+    /**
+     * Assign a teacher to a course..
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Course  $course
+     * @return \Illuminate\Http\Response
+     */
+    public function saveStudent(Request $request, Course $course)
+    {
+        $course->students()->sync($request->student_id);
+        return redirect()->route('courses.index');
     }
 
     /**
